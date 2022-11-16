d1c1081ebd3b1380f1164eaccd0819bb5b0efd05
BegRaj <begrajv2@gmail.com>
2022-11-16 21:54:58 +0530

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './storyComponent/home/home.component';
+import { AboutComponent } from './storyComponent/about/about.component';
+import { LoginComponent } from './storyComponent/login/login.component';
+import { RegisterComponent } from './storyComponent/register/register.component';
+import { IdeasComponent } from './storyComponent/videoIdea/ideas/ideas.component';
+import { AddIdeaComponent } from './storyComponent/videoIdea/add-idea/add-idea.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'register', component:RegisterComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'home', component:HomeComponent},
+  {path: 'ideas', component:IdeasComponent},
+  {path: 'add-idea', component:AddIdeaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './storyComponent/home/home.component';
+import { AboutComponent } from './storyComponent/about/about.component';
+import { LoginComponent } from './storyComponent/login/login.component';
+import { RegisterComponent } from './storyComponent/register/register.component';
+import { NavbarComponent } from './storyComponent/navbar/navbar.component';
+import { IdeasComponent } from './storyComponent/videoIdea/ideas/ideas.component';
+import { AddIdeaComponent } from './storyComponent/videoIdea/add-idea/add-idea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    IdeasComponent,
+    AddIdeaComponent
   ],
   imports: [
     BrowserModule,
